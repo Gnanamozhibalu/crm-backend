3ddf357f98b0f45a0031362c4a0b6260156797e3
Gnanamozhibalu <gnanamozhi.it@gmail.com>
2020-07-25 09:13:22 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const mongodb=require("mongodb");
+const bodyparser=require("body-parser");
+const url=""
+const app=express();
+app.use(bodyparser.json())
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  console.log(req.body);
+  res.render('index', { title: 'hello' });
+  try{
+   let client=await mongodb.connect(url);
+   let db=client.db("test");
+   let data= await db.collection("users").insertOne(req.body);
+   await client.close();
+  }catch(error){
+   console.log(error);
+  }
+});
+
+
+module.exports = router;
